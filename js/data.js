737fdd056d1fc5542969bfc69d1d3d73f9e68b7e
Timka09 <tymofij.i.naumenko@gmail.com>
2023-02-19 11:59:03 +0100

    замінив інформацію на себе

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const user = {
+  name: "Тимофій",
+  surname: "Науменко",
+  userPhoto: "тимоха против слендера.png",
+  qrQode: "qr-code.svg",
+  dreams: [
+    { src: "image.webp", alt: " подкрадули" },
+    { src: "берлога.png", alt: "спати" },
+    { src: "где я.png", alt: "на марс" },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  700,
+  800,
+  900,
+  1200,
+  1400,
+  1700,
+  2000,
+  2100,
+  2400,
+  3000,
+  3500,
+  3800,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+  { month: "Червень", skill: "силу" },
+  { month: "Липень", skill: "швидкість" },
+  { month: "Серпень", skill: "розум" },
+  { month: "Вересень", skill: "критичне мислення" },
+  { month: "Жовтень", skill: "дисцепліну" },
+  { month: "Листопад", skill: "матиматику" },
+  { month: "Грудень", skill: "англіську мову" },
+  { month: "Січень", skill: "впевненість" },
+  { month: "Лютий", skill: "Незламність" },
+  { month: "Березень", skill: "хитрість" },
+  { month: "Квітень", skill: "програмування" },
+  { month: "Травень", skill: "режим" },
+];
+
+export { user, arr, necessaryExpenses, todo };
